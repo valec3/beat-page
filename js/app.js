03f1c866c4738a6d6a395b51bbd2395bf3e2a6d2
Victor Raul <v1c70rotakmer@gmail.com>
2023-09-09 23:04:39 -0500

    Add section case, specs and sponsor

--- js/app.js
+++ js/app.js
@@ -12,8 +12,17 @@ navClose.addEventListener("click", () => {
     nav.classList.remove("active");
 })
 
-// Remove menu mobile
+// Remove menu mobile when scroll
 window.addEventListener("scroll", () => {
     navMenu.classList.remove("show-menu");
     nav.classList.remove("active");
 })
+
+function scrollHeader(){
+    const header = document.querySelector("#header");
+    if(this.scrollY >= 50) 
+        header.classList.add("scroll-header"); 
+    else 
+        header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
