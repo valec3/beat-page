6d4a538020351c1f565a6a7953599e861bfe8ed7
Victor Raul <v1c70rotakmer@gmail.com>
2023-11-23 22:52:24 -0500

    Add functionality to the scroll

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const navMenu = document.querySelector("#nav-menu"),
     navToggle = document.querySelector("#nav-toggle"),
     navClose = document.querySelector("#nav-close"),
-    nav = document.querySelector(".nav");
+    nav = document.querySelector(".nav"),
+    scrollUp = document.querySelector(".scroll-up");
 
 navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
@@ -31,3 +32,41 @@ window.addEventListener("scroll", scrollHeader);
 window.addEventListener("click", (e) => {
     e.preventDefault();
 });
+
+// scroll up active
+function scrollUpActive(){
+    if(this.scrollY >= 200)
+        scrollUp.classList.add("show-scroll");
+    else
+        scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUpActive);
+
+scrollUp.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+
+
+// Section active link
+
+const sections = document.querySelectorAll(".section");
+const navLink = document.querySelectorAll(".nav__link");
+
+function scrollActive(){
+    const scrollY = window.scrollY;
+    sections.forEach(section => {
+        const sectionHeight = section.offsetHeight;
+        const sectionTop = section.offsetTop - 50;
+        const sectionId = section.getAttribute("id");
+        console.log(sectionId);
+        if(!["home", "specs", "case","products"].includes(sectionId)) return;
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            navLink.forEach(link => {
+                link.classList.remove("active-link");
+            })
+            document.querySelector(`.nav__menu a[href*=${sectionId}]`).classList.add("active-link");
+        }
+    })
+}
+window.addEventListener("scroll", scrollActive);
